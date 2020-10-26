6e1dd149bca2d6c43939551de101294b3b81286f
Rodrigo N <rodrigo.gomes65@etec.sp.gov.br>
2020-10-26 19:37:38 -0300

    event listener adicionado, codigo do Pedro commitado junto

---
+++ codigosPedro/carrinho.js
@@ -0,0 +1,15 @@
+const cartTable = document.querySelector('#carrinho-tabela');
+const tableRows = cartTable.querySelectorAll('tbody tr:not(:last-child)');
+let totalPrice;
+
+function cartTableHandler() {
+    totalPrice = 0;
+    tableRows.forEach((el) => {
+        const input = el.querySelector('.cart-element-input').value;
+        const price = Number(el.querySelector('.cart-element-price').innerText);
+        totalPrice += price * input;    
+    });
+    console.log(totalPrice);
+}
+cartTable.addEventListener('change', cartTableHandler);
+
--- scripts/carrinho.js
+++ scripts/carrinho.js
@@ -1,22 +1,54 @@
-// selecionando todos os inputs da tabela
-const tabQuant = document.querySelectorAll('#carrinho-tabela tbody tr td:first-child input');
-//selecionando todos os preços da tabela
-const tabValores = document.querySelectorAll('#carrinho-tabela tbody tr td:nth-child(3)');
-var i = 0;
-var total = 0;
-//iterando para extrair os valores
+//OBS:. Créditos ao meu amigo Pedro que me ajudou a melhorar
+//esse código. Só traduzi pra PT-BR, mas irei commitar a versão dele
+//no monorepo
 
-for(i = 0; i<tabQuant.length; i++){
+//abaixo, o meu jeito de fazer o total dinâmico (excluí o meu listener),
+
+// selecionando todos os inputs da tabela
+const tabQuant = document.querySelectorAll('.carrinho-item-qnt');
+
+//selecionando todos os preços da tabela
+const tabValores = document.querySelectorAll('.carrinho-item-preco');
+
+var i = 0;
+let totalR = 0;
+
+//iterando para extrair os valores
+for(i = 0;i < tabQuant.length; i++){
     //soma = soma + (tabQuant[x] * tabValores[x]);
     let val = parseFloat(tabValores[i]['textContent']);
     let qnt = parseInt(tabQuant[i]['value']);
-    total = total + (qnt*val);
-   
-    
-
+    totalR = totalR + (qnt*val);
 }
-console.log(total);
+document.querySelector('#tabela-total').innerHTML = totalR;
+
+//----------------------//
+//aqui é o jeito do Pedro para fazer o Total dinâmico
+const carrinhoTabela = document.querySelector('#carrinho-tabela')
+const linhasTabela = carrinhoTabela.querySelectorAll('tbody tr:not(:last-child)');
+let total;
+//iterando para extrair os valores
+function carrinhoHandler(){
+    total = 0;
+    linhasTabela.forEach((el) => {
+        // selecionando todos os inputs da tabela
+        const qnt = el.querySelector('.carrinho-item-qnt').value;
+        //selecionando todos os preços da tabela
+        const valor = Number(el.querySelector('.carrinho-item-preco').innerText);
+        total += valor * qnt;    
+    });
+    console.log(total);
+    //total.innerHTML = soma;
+    document.querySelector('#tabela-total').innerHTML = total;
+}
+
+//event listener
+carrinhoTabela.addEventListener('change', carrinhoHandler);
 
 
-//total.innerHTML = soma;
-document.querySelector('#tabela-total').innerHTML = total;
+
+
+
+
+
+
